2ad73e0997142468e47083c0478458b8e1d521cd
Timor Tartakovsky <Timor.Tartakovsky@sapiens.com>
2019-09-15 22:29:16 +0300

    prod changes

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
-    remoteUrl : '<paste your Azure Cosmos DB connection string here',
+    remoteUrl : 'mongodb://packtcosmos-db:5RDzaQR4UoRKx6N9aVWCZAxjn5vsxbSj2h9hgc2Fk9u4H4yLwHjr1e9CXN06qRu7w8y0Bz6tZ6vSwkNgaFHBOg==@packtcosmos-db.documents.azure.com:10255/?ssl=true&replicaSet=globaldb',
     
     localUrl: 'mongodb://localhost/meanstacktutorials'
 };
